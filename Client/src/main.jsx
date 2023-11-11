6d39da52e26a90040f3a4679d31dd4b6951b4008
rishu <rishusingh9369@gmail.com>
2023-11-11 17:07:50 +0530

    changing node engines and url of client for backend

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import Contextfun from './ContextApi.jsx'
 
-export const server = `http://localhost:5000/api`;
+export const server = `https://todoapp-utx2.onrender.com/api`;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
